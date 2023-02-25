a00ae2c12bf240157e08c00a5d5ec4bee5b305d7
Sauce <telmobelasauce@ua.pt>
2023-02-25 14:04:58 +0000

    Color Detection

---
+++ Sample/OpenCV4x/build/intermediates/cxx/Debug/5j1s5e4l/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"D:\\UNi\\prog\\skd\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "E:\\uni\\3 ANO\\2sem\\ICM\\Sample\\OpenCV4x\\.cxx\\Debug\\5j1s5e4l\\arm64-v8a" ^
+  opencv_jni_shared
---
+++ Sample/OpenCV4x/build/intermediates/cxx/Debug/5j1s5e4l/logs/x86/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"D:\\UNi\\prog\\skd\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HE:\\uni\\3 ANO\\2sem\\ICM\\Sample\\OpenCV4x\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=x86" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86" ^
+  "-DANDROID_NDK=D:\\UNi\\prog\\skd\\ndk\\23.1.7779620" ^
+  "-DCMAKE_ANDROID_NDK=D:\\UNi\\prog\\skd\\ndk\\23.1.7779620" ^
+  "-DCMAKE_TOOLCHAIN_FILE=D:\\UNi\\prog\\skd\\ndk\\23.1.7779620\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=D:\\UNi\\prog\\skd\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=E:\\uni\\3 ANO\\2sem\\ICM\\Sample\\OpenCV4x\\build\\intermediates\\cxx\\Debug\\5j1s5e4l\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=E:\\uni\\3 ANO\\2sem\\ICM\\Sample\\OpenCV4x\\build\\intermediates\\cxx\\Debug\\5j1s5e4l\\obj\\x86" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BE:\\uni\\3 ANO\\2sem\\ICM\\Sample\\OpenCV4x\\.cxx\\Debug\\5j1s5e4l\\x86" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
