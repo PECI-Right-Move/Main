b3f790008c5cd998cdbc8e4f791abc9258692a9a
Sauce <telmobelasauce@ua.pt>
2023-03-03 13:27:17 +0000

    new stuff

--- Sample/OpenCV4x/build/intermediates/cxx/Debug/1r435t2v/logs/x86/build_command_opencv_jni_shared.bat
+++ Sample/OpenCV4x/build/intermediates/cxx/Debug/1r435t2v/logs/x86/build_command_opencv_jni_shared.bat
@@ -1,5 +1,5 @@
 @echo off
-"D:\\UNi\\prog\\skd\\cmake\\3.22.1\\bin\\ninja.exe" ^
+"C:\\Users\\LEGION\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
   -C ^
-  "E:\\uni\\3 ANO\\2sem\\ICM\\Sample\\OpenCV4x\\.cxx\\Debug\\1r435t2v\\x86" ^
+  "C:\\Users\\LEGION\\Documents\\GitHub\\Main\\Sample\\OpenCV4x\\.cxx\\Debug\\1r435t2v\\x86" ^
   opencv_jni_shared
